2df1a3245295fcb4618122301cfbcce3c42fb690
imKulkat <imkulkat@gmail.com>
2025-11-14 11:15:50 -0600

    Update game.js

--- game.js
+++ game.js
@@ -51,7 +51,7 @@ canvas.addEventListener('click', function(event) {
   const speed = 14;
   projectiles.push({
     x: player.x + player.width,
-    y: player.y - 25,
+    y: player.y + player.height * 0.6,
     dx: speed,
     dy: 0,
     exploding: false,
